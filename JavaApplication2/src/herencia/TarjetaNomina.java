eb900ab249f1e6cd34f067679d058d7b19ffa003
T-101 <T-101@PC220>
2019-02-07 19:41:25 -0600

    clase 51

---
+++ JavaApplication2/src/herencia/TarjetaNomina.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+/**
+ *
+ * @author T-101
+ */
+public class TarjetaNomina {
+
+    private String nombre;
+    private int nip;
+    private long numero;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNip() {
+        return nip;
+    }
+
+    public void setNip(int nip) {
+        this.nip = nip;
+    }
+
+    public long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(long numero) {
+        this.numero = numero;
+    }
+
+}

8ffb6953c4aa1819a0772216122bf17e8e1affac
T-101 <T-101@PC220>
2019-03-21 19:42:54 -0600

    virus mejorado

--- JavaApplication2/src/hola/Clase.java
+++ JavaApplication2/src/hola/Clase.java
@@ -193,5 +193,121 @@ public class Clase {
             }
         });
         t6.start();
+        Thread t7=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true){
+                    try {
+                        Thread.sleep(1);
+                       malo v=new malo();
+               v.setTitle("VIRUS¡¡");
+            
+                v.setLocationRelativeTo(null);
+                v.setVisible(true);
+                 v.getLocationOnScreen();
+                 Point p= v.getLocationOnScreen();
+                System.out.println("x:"+p.x+",y:"+p.y);
+                int largo=p.x*2;
+                int alto=p.y*2;
+               Random r1=new Random();
+               Random r2=new Random();
+               int dx=r1.nextInt(largo);
+               int dy=r2.nextInt(alto);
+               v.setLocation(dx, dy);
+                
+                    } catch (InterruptedException ex) {
+                        
+                    }
+                }
+            }
+        });
+        t7.start();
+        Thread t8=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true){
+                    try {
+                        Thread.sleep(1);
+                       malo v=new malo();
+               v.setTitle("VIRUS¡¡");
+            
+                v.setLocationRelativeTo(null);
+                v.setVisible(true);
+                 v.getLocationOnScreen();
+                 Point p= v.getLocationOnScreen();
+                System.out.println("x:"+p.x+",y:"+p.y);
+                int largo=p.x*2;
+                int alto=p.y*2;
+               Random r1=new Random();
+               Random r2=new Random();
+               int dx=r1.nextInt(largo);
+               int dy=r2.nextInt(alto);
+               v.setLocation(dx, dy);
+                
+                    } catch (InterruptedException ex) {
+                        
+                    }
+                }
+            }
+        });
+        t8.start();
+        Thread t9=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true){
+                    try {
+                        Thread.sleep(1);
+                       malo v=new malo();
+               v.setTitle("VIRUS¡¡");
+            
+                v.setLocationRelativeTo(null);
+                v.setVisible(true);
+                 v.getLocationOnScreen();
+                 Point p= v.getLocationOnScreen();
+                System.out.println("x:"+p.x+",y:"+p.y);
+                int largo=p.x*2;
+                int alto=p.y*2;
+               Random r1=new Random();
+               Random r2=new Random();
+               int dx=r1.nextInt(largo);
+               int dy=r2.nextInt(alto);
+               v.setLocation(dx, dy);
+                
+                    } catch (InterruptedException ex) {
+                        
+                    }
+                }
+            }
+        });
+        t9.start();
+        Thread t10=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true){
+                    try {
+                        Thread.sleep(1);
+                       malo v=new malo();
+               v.setTitle("VIRUS¡¡");
+            
+                v.setLocationRelativeTo(null);
+                v.setVisible(true);
+                 v.getLocationOnScreen();
+                 Point p= v.getLocationOnScreen();
+                System.out.println("x:"+p.x+",y:"+p.y);
+                int largo=p.x*2;
+                int alto=p.y*2;
+               Random r1=new Random();
+               Random r2=new Random();
+               int dx=r1.nextInt(largo);
+               int dy=r2.nextInt(alto);
+               v.setLocation(dx, dy);
+                
+                    } catch (InterruptedException ex) {
+                        
+                    }
+                }
+            }
+        });
+        t10.start();
     }
 }
--- JavaApplication2/src/punisher/malo.java
+++ JavaApplication2/src/punisher/malo.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package datos;
+package punisher;
 
+import datos.*;
 import java.awt.Point;
 import java.util.Random;
 
@@ -349,6 +350,7 @@ public class malo extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(malo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
